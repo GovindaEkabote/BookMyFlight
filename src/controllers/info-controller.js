c9c15898695fdc71b534250f38dd90cb16348978
GovindaEkbote <govindaekbote7@gmail.com>
2025-06-05 05:54:21 +0530

    Book My Flight 2

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,12 +1,13 @@
-const info = (req,res) =>{
-    return res.json({
-        success:true,
-        message:"API is live",
-        error:{},
-        data:{}
-    })
-}
+const { StatusCodes } = require('http-status-codes'); 
+const info = (req, res) => {
+    return res.status(StatusCodes.OK).json({
+        success: true,
+        message: "API is live",
+        error: {},
+        data: {}
+    });
+};
 
 module.exports = {
-     info
-}
+    info
+};
